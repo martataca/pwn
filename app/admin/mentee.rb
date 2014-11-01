b2b8f7b34ea1055a0563d48ef2418c0d71b51d9b
Marta Baptista <martacpnbaptista@gmail.com>
2014-11-01 19:07:14 +0000

    activeadmin new tables with views calling shows

--- app/admin/mentee.rb
+++ app/admin/mentee.rb
@@ -16,18 +16,48 @@ ActiveAdmin.register Mentee do
   
 
   index do
-    column "Picture" do |mentee|
-     image_tag mentee.profile.picture, class: 'profile_picture_thumbnail_xs'
+
+    column "Picture" do |user|
+      if Profile.exists?(:user_id => user.id)
+        image_tag user.profile.picture, class: 'profile_picture_thumbnail_xs'
+      end
     end
+
     column "First Name", :firstName
     column "Last Name",  :lastName
     column :email
-    column "Phone Number" do |mentee|
-      mentee.profile.phone_number
+
+
+    column "Phone Number" do |user|
+      if Profile.exists?(:user_id => user.id)
+        user.profile.phone_number
+      end
     end
 
+    column "Company" do |user|
+      if Profile.exists?(:user_id => user.id)
+        user.profile.company
+      end
+    end
+
+    column "Application Completeness" do |user|
+      if Profile.exists?(:user_id => user.id)
+        user.profile.percentage
+      end
+    end
+
+
+
+
+
     actions :defaults => false do |user|
-      link_to "View", admin_profile_path(user)
+      if Profile.exists?(:user_id => user.id)
+        @iduser = user.id
+        @profile = Profile.where(user_id: @iduser).first
+        link_to "View", admin_profile_path(@profile.id)
+      else
+        link_to "View", admin_user_path(user)
+      end
     end
   end
  
