2b2d74e1bc3ecb5bc8bea421474279697508338a
Nuno Duarte <etraudonun@gmail.com>
2014-11-04 21:46:15 +0000

    Backup recovery mechanism added;

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
 
+    def backup
+      @ls = `ls`
+           
+      `rsync -aplx --link-dest=pwn/public/uploads/profile/picture/ public/uploads/profile/picture/ tmp/merge/`
+      `rsync -aplx --link-dest=pwn/public/uploads/profile/cv/ public/uploads/profile/cv/ tmp/merge/`      
+      `zip -r backup.zip tmp/merge/`
+      
+      send_file "backup.zip", :type => 'application/zip', :disposition => 'attachment', :filename => "backup.zip"
+      `rm -rf tmp/merge/`
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'contacts' => 'home#contacts'
   get 'mentoring' => 'home#mentoring'
  
-
+  get 'backup' => 'home#backup'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
